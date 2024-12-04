0622f670ce33d897c0f098bd1edb79573f96c162
Pavel <pavel@b2profit.ru>
2024-12-04 20:05:09 +0300

    1 лекция

--- 1_Lect/script.js
+++ 1_Lect/script.js
@@ -15,9 +15,31 @@ console.log(Math.min(...arr)); // -2
 // function sumTwo(num1, num2, ...rest) {
 //     console.log(rest);
 //     return num1 + num2;
-// }
+// } // Записал в файл sum.js
 
 console.log(sumTwo(...arr));
+
+// Примеры:
+console.log('Пример 1');
+// Напишем не очень удобную, но показательную функцию, которая умеет принимать неограниченное число аргументов, и находить максимум среди них. Функция должна вызываться подобным образом: const maximum = findMax(4, 7, 10);
+
+function findMax() {
+    const values = arguments; // arguments- переменная доступная внутри каждой функции, которая содержит в себе все аргументы, переданные в функцию. Является псевдомассивом. 
+    let maxValue = -Infinity; // Так как arguments является псевдомассивом, мы не можем применять к нему новые методы массивов такие как forEach или reduce, поэтому будем итерировать по старинке. 
+    for (let index = 0; index < values.length; index++) {
+        if (values[index] > maxValue) maxValue = values[index];
+    }
+    return maxValue;
+};
+
+const group1PracticeTime = studentsGroup1PracticeTime.map((student) => student.practiceTime);
+console.log(group1PracticeTime);
+const group2PracticeTime = studentsGroup2PracticeTime.map((student) => student.practiceTime);
+console.log(group2PracticeTime);
+
+console.log('Максимальная длительность практики в 1 группе: ' + findMax(...group1PracticeTime));
+console.log('Максимальная длительность практики в 2 группе: ' + findMax(...group2PracticeTime));
+
 
 
 
---
+++ 1_Lect/spred.js
@@ -0,0 +1,41 @@
+const studentsGroup1PracticeTime = [
+    {
+        firstName: "Ivanov",
+        practiceTime: 56
+    },
+    {
+        firstName: "Petrov",
+        practiceTime: 120
+    },
+    {
+        firstName: "Sidorov",
+        practiceTime: 148
+    },
+    {
+        firstName: "Belkin",
+        practiceTime: 20
+    },
+    {
+        firstName: "Avdeev",
+        practiceTime: 160
+    }
+];
+
+const studentsGroup2PracticeTime = [
+    {
+        firstName: "Mankov",
+        practiceTime: 87
+    },
+    {
+        firstName: "Kistin",
+        practiceTime: 133
+    },
+    {
+        firstName: "Kotlyarov",
+        practiceTime: 140
+    },
+    {
+        firstName: "Peskov",
+        practiceTime: 10
+    }
+];
