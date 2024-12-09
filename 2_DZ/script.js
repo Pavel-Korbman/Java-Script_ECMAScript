c2bc43f09d8c94a53f0ac09336e6aad0971b154f
Pavel <pavel@b2profit.ru>
2024-12-09 17:28:49 +0300

    2 ДЗ

---
+++ 2_DZ/script.js
@@ -0,0 +1,20 @@
+class Book {
+    constructor(title, author, pages) {
+        this.title = title; this.author = author;
+        this.pages = pages;
+    }
+    displayInfo() {
+        console.log(`Название: ${this.title}, Автор: ${this.author}, Страниц: ${this.pages}`);
+    }
+}
+
+class Student {
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    displayInfo() {
+        console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`);
+    }
+}
