ce1e0ea09bfbf2808518b7202230dee1c8db505a
Pavel <pavel@b2profit.ru>
2024-12-16 19:54:51 +0300

    4 ДЗ

--- 4_DZ/script.js
+++ 4_DZ/script.js
@@ -11,19 +11,74 @@
 // 3. Верните объект с данными о пользователе. 
 // 4. Если ответ не успешен, отклоните промис с сообщением об ошибке.
 
-// const getUserData  = async (url) => {  
-//     const response = await fetch(url);  
-//     const user = await response.text();
-//     console.log(user);
-// }
 
-const getUserData  = async (id) => {  
+const getUserData = async (id) => {
     const response = await fetch(`https://api.github.com/users`);
     if (response.status == 200) {
         const userData = await response.json();
-        return (userData[id-1])? userData[id-1] : 'Пользователь не найден';       
+        return (userData[id - 1]) ? userData[id - 1] : 'Пользователь не найден';
     } else {
-        return `Error ${response.status}: ${response.statusText}`;        
+        return `Error ${response.status}: ${response.statusText}`;
     }
 }
 console.log(getUserData(1));
+
+// Задание 2: "Отправка данных на сервер" 
+// Реализуйте функцию для отправки данных о пользователе на сервер: 
+// Функция saveUserData 
+// ● Описание: 
+// ○ Функция принимает объект с данными 
+// о пользователе и использует fetch для отправки данных на удаленный сервер. 
+// ○ Функция возвращает промис, который разрешается, если данные успешно отправлены. 
+// ○ Если запрос неуспешен, промис должен быть отклонен с соответствующим сообщением об ошибке. 
+// ● Подсказка: 
+// ○ Используйте метод POST и задайте заголовок Content-Type как application/json. 
+// ○ Объект с данными о пользователе необходимо сериализовать в JSON-строку с помощью JSON.stringify().
+
+// Пример использования функции
+// const user = { name: 'John Smith', age: 30, email: 'john@example.com' }; saveUserData(user)
+//     .then(() => { console.log('User data saved successfully'); })
+//     .catch(error => { console.error(error); });
+
+const saveUserData = async (obj) => {
+    try {
+        const response = await fetch('https://httpbin.org/post', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify(obj),
+        });
+        if (!response.ok) {
+            throw new Error('Failed to save');
+        } else { return response }
+
+    } catch (error) { return Promise.reject(error.message); }
+
+}
+
+const user = {
+    name: 'John Smith',
+    age: 30,
+    email: 'john@example.com'
+};
+console.log(saveUserData(user));
+saveUserData(user)
+    .then(() => { console.log('User data saved successfully'); })
+    .catch(error => { console.error(error); });
+
+// Задание 3: "Изменение стиля элемента через заданное время" 
+// Реализуйте функцию, которая изменяет стиль элемента с задержкой: 
+// Функция changeStyleDelayed 
+// ● Описание: 
+// ○ Функция принимает идентификатор элемента (id) и время задержки в миллисекундах (delay). 
+// ○ После истечения времени задержки, функция должна изменить стиль элемента. 
+// Пример использования функции 
+// changeStyleDelayed('myElement', 2000); // Через 2 секунды изменяет стиль элемента с id 'myElement'
+
+function changeStyleDelayed(idEl, delay) {
+    setTimeout(() => {
+        const element = document.getElementById(idEl);
+        (element) ? element.style.color = 'green' : console.log('Элемент не найден');
+    }, delay);
+}
+
+changeStyleDelayed('title', 2000);
