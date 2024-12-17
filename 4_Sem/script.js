e145b9c5f72cc0a662dd8d6a90e9cd5ee27bbda8
Pavel <pavel@b2profit.ru>
2024-12-17 22:58:07 +0300

    4 семинар

--- 4_Sem/script.js
+++ 4_Sem/script.js
@@ -25,4 +25,27 @@ const taskPlaner = (list) => {
     });
 }
 
-taskPlaner(tasks);
+//taskPlaner(tasks);
+
+// Задание 3 
+// Напишите программу, которая загружает данные с сервера с использованием объекта XMLHttpRequest 
+// 1. Создайте функцию loadData(url), которая принимает аргумент url (строка) - адрес сервера для загрузки данных. 
+// 2. Внутри функции loadData() создайте объект XMLHttpRequest с помощью new XMLHttpRequest(). 
+// 3. Зарегистрируйте обработчик события onreadystatechange, который будет вызываться при изменении состояния запроса. Проверьте, если успешно выполнен запрос и успешный статус ответа сервера, то выведите полученные данные в консоль с помощью console.log(xhr.responseText). 
+// 4. Откройте запрос с помощью xhr.open("GET", url, true). 5. Отправьте запрос на сервер.
+
+loadData = (url) => {
+    const xhr = new XMLHttpRequest(); 
+    xhr.open('GET', url, true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status != 200) {
+            console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            console.log(`user: ${xhr.responseText}`);
+            return xhr.response;
+        }
+    }  
+
+}
+loadData('https://api.github.com/users/octocat');
